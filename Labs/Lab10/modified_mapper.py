9b59fef8925e37fbc6a0722687efdc3b33372d94
mlichti@gmail.com <mlichti@gmail.com>
2014-11-08 00:32:41 -0800

    changed syntax in lab 10 mapper

--- Labs/Lab10/modified_mapper.py
+++ Labs/Lab10/modified_mapper.py
@@ -13,4 +13,4 @@ for line in sys.stdin:
         # Reduce step, i.e. the input for reducer.py
         #
         # tab-delimited; the trivial word count is 1
-        print '%s\t%s' % (word, 1)
+        print '{0}\t{1}'.format(word, 1)
